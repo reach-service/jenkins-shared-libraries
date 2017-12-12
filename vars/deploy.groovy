f9e648cbd3e330ee106ec96ea59fe1bbd7f9f0a6
David Michon <david.michon@gmail.com>
2017-12-12 09:51:44 -0300

    send deploy failure message before rolling back service

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -3,8 +3,8 @@ def call(String project, String stackName, String serviceName) {
       try {
         sh "docker service update --image reach/${project}:${currentBuild.displayName} ${stackName}_${serviceName} --detach=false"
       } catch (e) {
-        sh "docker service update --rollback ${stackName}_${serviceName} --detach=false"
         deployFailureAlert(${serviceName})
+        sh "docker service update --rollback ${stackName}_${serviceName} --detach=false"
         error "Failed to update the service ${serviceName}"
       }
     }
