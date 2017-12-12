d5a746ad3c646d88b81a18187a92d88b5fbca92b
David Michon <david.michon@gmail.com>
2017-12-12 10:19:42 -0300

    only build test image if Dockerfile.test exists

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,7 @@
 def call(String project) {
     sh "docker image build -t reach/${project}:latest -f Dockerfile ."
     sh "docker image tag reach/${project}:latest reach/${project}:${currentBuild.displayName}"
-    sh "docker image build -t reach/${project}-test -f Dockerfile.test ."
+    if (fileExists('Dockerfile.test')) {
+        sh "docker image build -t reach/${project}-test -f Dockerfile.test ."
+    }
 }
