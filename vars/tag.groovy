314b7f2c318f8a4f771a9e8e1a0603993c67f50f
David Michon <david.michon@gmail.com>
2017-12-12 13:35:03 -0300

    add git tagging

---
+++ vars/tag.groovy
@@ -0,0 +1,10 @@
+def call(String repo) {
+  withCredentials([usernamePassword(
+      credentialsId: "devreach",
+      usernameVariable: "USER",
+      passwordVariable: "PASS"
+  )]) {
+      sh "git tag -a ${currentBuild.displayName} -m ${currentBuild.displayName}"
+      sh "git push https://${USER}:${PASS}@reach-service/${repo} --tags"
+  }
+}
