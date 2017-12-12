6ff231d6b18d9bda0315c70a655f7a0b6dc03461
David Michon <david.michon@gmail.com>
2017-12-12 09:41:20 -0300

    add environment variable in deploy failure message

--- vars/deployFailureAlert.groovy
+++ vars/deployFailureAlert.groovy
@@ -1,7 +1,7 @@
-def call(String serviceName) {
+def call(String serviceName, String environment) {
     slackSend(
             color: "danger",
-            message: """${serviceName}:${currentBuild.displayName} could not be deployed at ${STAGE_NAME}.
+            message: """${serviceName}:${currentBuild.displayName} could not be deployed in ${environment}.
 Please check Jenkins logs for the job ${env.JOB_NAME} #${env.BUILD_NUMBER}
 ${env.RUN_DISPLAY_URL}"""
     )
