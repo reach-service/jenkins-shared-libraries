4fe0d74d724f817be2117d91e4de5c5eccefffb8
David Michon <david.michon@gmail.com>
2017-12-12 09:53:55 -0300

    fix failure alert invocation

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -3,7 +3,7 @@ def call(String project, String stackName, String serviceName) {
       try {
         sh "docker service update --image reach/${project}:${currentBuild.displayName} ${stackName}_${serviceName} --detach=false"
       } catch (e) {
-        deployFailureAlert(${serviceName})
+        deployFailureAlert("${serviceName}")
         sh "docker service update --rollback ${stackName}_${serviceName} --detach=false"
         error "Failed to update the service ${serviceName}"
       }
