b6bfd74e1bb8c67a427a3698c07476d54c1f5ee0
David Michon <david.michon@gmail.com>
2017-12-18 13:19:40 -0300

    add line feed

--- vars/updateVersion.groovy
+++ vars/updateVersion.groovy
@@ -1,6 +1,6 @@
 def call(filename) {
   if (fileExists(${filename}) {
     f = new File(${filename})
-    file.write "version=${env.BUILD_NUMBER}"
+    file.write "version=${env.BUILD_NUMBER}\n"
     }
 }
